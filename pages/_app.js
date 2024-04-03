4d16fb2914fb3c186a12bc416a5919039bf8500e
murali1466 <muralikrishnatangella1466@gmail.com>
2024-04-03 07:30:05 +0530

    session

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
-import { SessionContext, SessionProvider } from "next-auth/react"
+import {SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  return <SessionProvider>
+  return <SessionProvider session={pageProps.session}>
     {getLayout(<Component {...pageProps} />)}
   </SessionProvider>;
 }
--- pages/index.js
+++ pages/index.js
@@ -155,6 +155,8 @@ export async function getServerSideProps(context){
     }
   }
   return {
-    props :{}
+    props :{
+      session : session
+    }
   }
 }
